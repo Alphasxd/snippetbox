f816866b03d776ff8d14b2c6e9edb8ad92b99420
Sheldon <alphasxd@outlook.com>
2023-07-13 12:55:38 +0800

    add the current year

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 // serverError() helper 向 errorLog 写入错误信息，并向用户返回 500 Internal Server Error
@@ -26,6 +27,14 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultData (td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	// 从 templateCache 获取指定名称的模板，如果不存在，则调用 serverError() helper
 	ts, ok := app.templateCache[name]
@@ -37,8 +46,8 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// 创建一个缓冲区，然后执行模板，将渲染的结果写入缓冲区
 	buf := new(bytes.Buffer)
 
-	// 将存储在 templateData 中的动态数据写入缓冲区
-	err := ts.Execute(buf, td)
+	// 将存储在 templateData 中的动态数据写入缓冲区，同时将当前的年份信息写入缓冲区
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
