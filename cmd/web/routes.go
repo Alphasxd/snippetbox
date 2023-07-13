12e90a372e49dd2caf9398eb974dd4c6e03ca0bf
Sheldon <alphasxd@outlook.com>
2023-07-13 17:58:28 +0800

    updated routes with alice a middleware tool

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
+
 
 func (app *application) routes() http.Handler {
+
+	// 创建一个包含标准中间件的的中间件链，将会应用到每一个请求上。
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -11,5 +20,5 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
