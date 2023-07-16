be8515daf21b339bc5d413dc620c2d87a9d22e12
Sheldon <alphasxd@outlook.com>
2023-07-16 14:57:08 +0800

    updated: add requireAuthen method

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -6,6 +6,8 @@ import (
 )
 
 func secureHeaders(next http.Handler) http.Handler {
+
+	// 设置希望添加到响应中的标准的安全标头
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
@@ -15,6 +17,8 @@ func secureHeaders(next http.Handler) http.Handler {
 }
 
 func (app *application) logRequest(next http.Handler) http.Handler {
+
+	// logRequest 中间件将所有请求的远程地址和 HTTP 方法记录到应用的日志中
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 
@@ -23,6 +27,8 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 }
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
+
+	// 恢复 panic，如果发生 panic，则将堆栈跟踪信息写入日志，可以防止应用程序崩溃
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
@@ -31,6 +37,22 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			}
 		}()
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+
+	// 鉴定用户是否已经通过身份验证
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 如果用户没有通过身份验证，将用户重定向到登录页面
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		
+		// 否则，将 header 中的 Cache-Control 字段设置为 no-store，这样用户每次访问受保护的页面时，都会向服务器发送请求
+		w.Header().Add("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
 	})
 }
